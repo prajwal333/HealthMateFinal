80fee03f81bfbc6a2fd12685b7ce531b42d9f7a4
shrisab <shrisab12@gmail.com>
2023-05-29 21:52:42 +0545

    updated

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -1,8 +1,7 @@
-from django .urls import path
-from .import views
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns=[
+from django.urls import path
+from healthapp import views
+
+urlpatterns = [
     path("",views.index,name="healthapp"),
     path("about/",views.about,name="AboutUs"),
     path("consultationform/",views.consultationform,name="consultationform"),
